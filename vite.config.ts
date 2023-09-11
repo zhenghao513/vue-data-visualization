6805a8374073db6874d5965608be12ef25166ccf
zhenghao <zhengh513@outlook.com>
2023-09-11 21:20:35 +0800

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({}),
+    AutoImport({
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+      imports: [
+        'vue',
+        'vue-router',
+        {
+          axios: [['default', 'axios']],
+        },
+        { from: 'vue-router', imports: ['RouteLocationRaw'], type: true },
+      ],
+      defaultExportByFilename: false,
+      dirs: [],
+      dts: './auto-imports.d.ts',
+      vueTemplate: false,
+      resolvers: [],
+      injectAtEnd: true,
+      eslintrc: {
+        enabled: false,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true,
+      },
+    }),
+  ],
+  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+});
