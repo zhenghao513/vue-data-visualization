39627f6fa07f74ee249aa1d2462cc18db9d0fbf7
zhenghao <zhengh513@outlook.com>
2023-09-12 10:33:37 +0800

    refactor: 定义 Axios 请求的返回类型

--- src/api/visualization.ts
+++ src/api/visualization.ts
@@ -1,3 +1,79 @@
+export interface Region {
+  id: number;
+  name: string;
+  value: number;
+}
+
+export interface Server {
+  id: number;
+  name: string;
+  value: number;
+}
+
+export interface Risk {
+  id: number;
+  name: string;
+  value: number;
+}
+
+export interface Abnormal {
+  id: number;
+  name: string;
+  value: number;
+}
+
+export interface Relation {
+  id: number;
+  name: string;
+  source: string;
+  target: string;
+  speed: number;
+  value: [number, number];
+}
+
+export interface WordCloudData {
+  datas: Array<{ value: number; name: string }>;
+}
+
+export interface MapData {
+  voltageLevel: string[];
+  colors: string[];
+  categoryData: {
+    [key: string]: Array<{ name: string; value: number }>;
+  };
+}
+
+export interface TotalData {
+  total: string;
+  hb: string;
+  db: string;
+  hd: string;
+  zn: string;
+  xn: string;
+  xb: string;
+}
+
+export interface VisualizationResult {
+  regionData: {
+    regions: Region[];
+  };
+  serverData: {
+    servers: Server[];
+  };
+  riskData: {
+    risks: Risk[];
+  };
+  abnormalData: {
+    abnormals: Abnormal[];
+  };
+  relationData: {
+    relations: Relation[];
+  };
+  wordCloudData: WordCloudData;
+  mapData: MapData;
+  totalData: TotalData;
+}
+
 export function getVisualization() {
-  return axios.get('/visualization');
+  return axios.get<VisualizationResult, VisualizationResult>('/visualization');
 }
